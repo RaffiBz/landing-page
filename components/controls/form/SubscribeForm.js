b3917996fd3932ce90d93221a4e5116e951d07f8
Raffi Bzdigian <raffi.bzdigian@aimtech.am>
2022-09-16 15:20:11 +0400

    subscribe email sent / images png

--- components/controls/form/SubscribeForm.js
+++ components/controls/form/SubscribeForm.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
+import emailjs from "@emailjs/browser";
 
 import mail from "../../../public/images/general/mail.svg";
 import SubscribeButton from "../button/SubscribeButton";
 
 export default function SubscribeForm() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_nn1e4sq",
+        "template_ntqs9ps",
+        form.current,
+        "X10KuT4R9a96rbcQ7"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("subscribed successfully");
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <form className="flex justify-between border-2 border-transparent focus-within:border-opacity-30 focus-within:border-primary hover:border-blueStroke rounded-[0.938rem] bg-grayLight w-full max-w-[32.25rem] mx-auto  py-2 pl-4 pr-2 mt-8 md:mt-[1.563rem]">
+    <form
+      ref={form}
+      onSubmit={sendEmail}
+      className="flex justify-between border-2 border-transparent focus-within:border-opacity-30 focus-within:border-primary hover:border-blueStroke rounded-[0.938rem] bg-grayLight w-full max-w-[32.25rem] mx-auto  py-2 pl-4 pr-2 mt-8 md:mt-[1.563rem]"
+    >
       <div className="flex gap-1">
         <Image src={mail} width={16} height={16} alt="mail" />
         <input
           type="email"
+          name="user_email"
           className="outline-none bg-transparent placeholder:text-grayBg"
           placeholder="Enter your email here"
         />
--- data/SolutionsPage.js
+++ data/SolutionsPage.js
@@ -27,19 +27,19 @@ export const IndustryData = [
   {
     id: 1,
     title: "Health & Wellness",
-    img: "/images/solutions/health.svg",
+    img: "/images/solutions/health.png",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis vulputate ultrices augue cursus amet, id at nec. Sollicitudin convallis amet, consequat, volutpat ut vulputate nam etiam nisl. Lorem risus fermentum tellus quam et. Nibh feugiat turpis rhoncus etiam purus. At posuere tellus duis quis sagittis vel pellentesque.",
   },
   {
     id: 2,
     title: "Food & Beverage",
-    img: "/images/solutions/food.svg",
+    img: "/images/solutions/food.png",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis vulputate ultrices augue cursus amet, id at nec. Sollicitudin convallis amet, consequat, volutpat ut vulputate nam etiam nisl. Lorem risus fermentum tellus quam et. Nibh feugiat turpis rhoncus etiam purus. At posuere tellus duis quis sagittis vel pellentesque.",
   },
   {
     id: 3,
     title: "Fashion & Beauty",
-    img: "/images/solutions/fashion.svg",
+    img: "/images/solutions/fashion.png",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis vulputate ultrices augue cursus amet, id at nec. Sollicitudin convallis amet, consequat, volutpat ut vulputate nam etiam nisl. Lorem risus fermentum tellus quam et. Nibh feugiat turpis rhoncus etiam purus. At posuere tellus duis quis sagittis vel pellentesque.",
   },
   {
